99fc5bfde138ee18aec3ba1d5d2724dd3551b1f4
Nimrod Acosta <52327093+nimplay@users.noreply.github.com>
2025-01-14 16:00:47 -0400

    create models

--- website/models.py
+++ website/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+
+def validate_three_digits(value):
+    if not (100 <= value <= 999):
+        raise ValidationError('The value must be a three-digit number.')
 
 # Create your models here.
+class Record(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    deleted_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=50)
+    last_name  = models.CharField(max_length=50)
+    email  = models.CharField(max_length=100)
+    phone  = models.CharField(max_length=50)
+    address  = models.CharField(max_length=200)
+    city  = models.CharField(max_length=50)
+    state  = models.CharField(max_length=50)
+    zipcode  = models.CharField(max_length=50)
+    age = models.PositiveIntegerField(validators=[validate_three_digits])
+
+    def __str__(self):
+        return (f"{self.first_name} {self.last_name}")
+
