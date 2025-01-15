2171bbe6903a831356da7dcc4e80f2e65dadb1a1
Nimrod Acosta <52327093+nimplay@users.noreply.github.com>
2025-01-14 20:53:52 -0400

    update record logic

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 def home(request):
@@ -68,4 +68,27 @@ def delete_record(request, pk):
         return redirect('home')
 
 def add_record(request):
-     return render(request, 'add_record.html', {'add_record': add_record} )
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Record Saved!")
+                return redirect('home')
+        return render(request, 'add_record.html', {'form': form} )
+    else:
+        messages.success(request, "You Must Be Logged In...")
+        return redirect('home')
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Record Updated!")
+            return redirect('home')
+        return render(request, 'update_record.html', {'form': form} )
+    else:
+        messages.success(request, "You Must Be Logged In...")
+        return redirect('home')
